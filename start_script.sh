5b128d462819b6baef7e11fdc48ea21122d0796c
schnoog <schnoog@schnoog.eu>
2018-12-07 23:16:16 +0100

    fast commit

--- start_script.sh
+++ start_script.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-script="$SCRIPT_DIR""/mqtt_sensors.sh"
-screen -dmS mqttpub "$script" -v &
+script1="$SCRIPT_DIR""/mqtt_sensors.sh"
+script2="$SCRIPT_DIR""/mqtt_callback.sh"
 
-script="$SCRIPT_DIR""/mqtt_callback.sh"
-screen -dmS mqttsub "$script" -v &
+pid1=$(screen -ls mqttpub | grep "tached" | awk '{print$1}')
+if [ "$pid1" == "" ]
+then
+    screen -dmS mqttpub "$script1" -v &
+    echo "Publisher started"
+else
+    screen -X -S $pid1 quit
+    screen -dmS mqttpub "$script1" -v &
+    echo "Publisher restarted"
+fi
+pid1=$(screen -ls mqttsub | grep "tached" | awk '{print$1}')
+if [ "$pid1" == "" ]
+then
+    screen -dmS mqttsub "$script2" -v &
+    echo "Callback started"
+else
+    screen -X -S $pid1 quit
+    screen -dmS mqttsub "$script2" -v &
+    echo "Callback restarted"
+fi
