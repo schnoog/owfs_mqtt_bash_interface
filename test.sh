8edbf8993ba025da0b21a15798e352881edf48c1
schnoog <schnoog@schnoog.eu>
2018-12-14 21:34:05 +0100

    fast commit

--- test.sh
+++ test.sh
@@ -6,4 +6,35 @@ echo $SCRIPT_DIR
 source settings.cfg
 source functions.sh
 
-PublishSingleDevice "26.33D984000003"
+#R8= 3A.8C3518000000 - B
+#R7= 3A.8C3518000000 - A
+#R6= 3A.D43518000000 - B
+
+
+
+function GetLTI {
+r8=$(owget 3A.8C3518000000/sensed.B)
+r7=$(owget 3A.8C3518000000/sensed.A)
+r6=$(owget 3A.D43518000000/sensed.B)
+if [ "$r8" == "1" ]
+then
+    lvl=4
+else
+    if [ "$r7" == "1" ]
+    then
+        lvl=3
+    else
+        if [ "$r6" == "1" ]
+        then
+            lvl=2
+        else
+            lvl=1
+        fi
+    fi
+fi 
+echo $lvl
+}
+
+
+
+GetLTI
